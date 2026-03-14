d6eac8ce1754a719ae7c7988c36b8826024a9f11
Hassoun-Edu-Project <hassoun.mohamed993@gmail.com>
2026-03-14 23:54:28 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -55,8 +55,8 @@ with st.sidebar:
 st.title("🌟 منصة الوثائق التربوية")
 
 if choice == "الرئيسية":
-    st.subheader("مرحباً بك أستاذ محمد")
-    st.info("منصة متكاملة لدعم مربي التعليم الأولي بالمغرب.")
+    st.subheader("مرحباً بكم في منصة Hassoun-Edu") # هذا التغيير ليناسب الجميع
+    st.info("فضاء تربوي مخصص لتقاسم الوثائق والدلائل الخاصة بمربي التعليم الأولي بالمغرب.")
     st.image("https://img.freepik.com/free-vector/happy-kids-classroom-scene_1308-27158.jpg", use_column_width=True)
 
 elif choice == "المذكرة اليومية":
