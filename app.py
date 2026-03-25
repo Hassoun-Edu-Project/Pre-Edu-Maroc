e1bbfbb8685d009efd91b831f0a1271dcf7bd290
Hassoun-Edu-Project <hassoun.mohamed993@gmail.com>
2026-03-25 23:54:25 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ st.set_page_config(
 )
 
 # 2. إعدادات الأمان وكلمة المرور
-ADMIN_PASSWORD = st.secrets["youghrtamassinanas1993"]
+ADMIN_PASSWORD = "Hassoun_Pro_2026" 
 
 if 'logged_in' not in st.session_state:
     st.session_state['logged_in'] = False
